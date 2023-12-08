48ac6fb1ee783a5d6972f78e8762d634a0cbd1ef
Bernhard Holzfeind <bernhard.holzfeind@gmail.com>
2023-12-08 22:17:24 +0100

    auto restart, fixing open files error

--- launcher.sh
+++ launcher.sh
@@ -1,10 +1,12 @@
-:q!/bin/sh
+!/bin/sh
 # launcher.sh
 # navigate to home directory, then to this directory, then execute python script, then back home
 
 cd /
 cd /home/michael/ton-o-mat
-source  /home/michael/ton-o-mat/env/bin/activate
-#fluidsynth -a alsa -m alsa_seq -i -l -s -g 1.0 /usr/share/sounds/sf2/FluidR3_GM.sf2 &
-python /home/michael/ton-o-mat/src/manageToken.py 
-cd /
+while true
+do
+    pipenv run /home/michael/ton-o-mat/env/bin/python /home/michael/ton-o-mat/src/manageToken.py
+    echo oops, restarting...
+done
+
